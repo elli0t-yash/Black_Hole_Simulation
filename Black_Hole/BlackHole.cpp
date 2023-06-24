c3755556025a474912cc9e4b916c11017a27003d
Yash Mishra <yash01012002@gmail.com>
2023-06-25 01:51:28 +0530

    python_blackhole

--- Black_Hole/BlackHole.cpp
+++ Black_Hole/BlackHole.cpp
@@ -1,9 +1,33 @@
-#include<GL/gl.h>
-#include<GL/glut.h>
+#include <GL/gl.h>
+#include <GL/glut.h>
 
-int windowWidth = 800;
-int windowHeight = 600;
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
 
-void reshape(int width, int height) {
-	glViewport(0, 0, width, height);
+    // Set up the camera position
+    gluLookAt(0.0, 0.0, 5.0,  // eye position
+        0.0, 0.0, 0.0,  // look-at position
+        0.0, 1.0, 0.0); // up direction
+
+    // Draw a sphere
+    glColor3f(1.0, 1.0, 1.0); // Set sphere color (white)
+    glutSolidSphere(1.0, 50, 50); // Parameters: radius, slices, stacks
+
+    glutSwapBuffers();
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+    glutInitWindowSize(800, 600);
+    glutCreateWindow("Sphere Example");
+
+    glClearColor(0.0, 0.0, 0.0, 1.0);  // Set clear color to black
+    glEnable(GL_DEPTH_TEST);           // Enable depth testing
+
+    glutDisplayFunc(display);
+    glutMainLoop();
+
+    return 0;
 }
